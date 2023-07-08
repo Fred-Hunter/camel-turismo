08a2918caab337b99c644f0585ccc4699cd6efcc
Josh Davis <josh.davis@enable.com>
2023-07-08 10:45:09 +0100

    Add camel level setuff

--- skills/camel-skill.ts
+++ skills/camel-skill.ts
@@ -1,6 +1,10 @@
 class CamelSkill {
-    constructor(private readonly _name: string) {
-        
+    constructor(
+        private readonly _name: string, 
+        private readonly _level: number) 
+    {
+        const xp = this.getXpRequiredForVirtualLevel(_level);
+        this._currentXp = xp;
     }
 
     private readonly _minSkillLevel = 1;
@@ -10,9 +14,17 @@ class CamelSkill {
     public get name() {
         return this._name;
     }
+
+    private getXpRequiredForVirtualLevel(level: number) {
+        return (level - 1) * 100;
+    }
+
+    private getVirtualLevelWithXp(xp: number) {
+        return Math.floor(xp / 100) + 1;
+    }
     
     public get level(): number {
-        const virtualLevel = Math.floor(this._currentXp / 100);
+        const virtualLevel = this.getVirtualLevelWithXp(this._currentXp);
         if (virtualLevel <= this._minSkillLevel) {
             return this._minSkillLevel;
         }
