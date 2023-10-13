7f2f8d2596c586e25a2cdfda1474a7e880348bee
Fred-Hunter <fredski.1+fredgithub@hotmail.co.uk>
2023-10-13 14:16:52 +0100

    Add new camel pool

--- global/camel-stable.ts
+++ global/camel-stable.ts
@@ -1,8 +1,12 @@
 class CamelStable {
     constructor(
         private readonly _camelCreator: CamelCreator
-        ) {
+    ) {
     }
+
+    private _seedRadix = 36;
+    private _camelInformationLength = 10;
+    private _numberOfCamels = 25;
 
     public populateStable() {
         let firstTimeSetUp = false;
@@ -12,65 +16,54 @@ class CamelStable {
         }
 
         const seed = GameState.stableSeed;
-        const camelInformationLength = 10;
-        
+
         let index = 0;
         let camels: Camel[] = [];
 
-        if (!firstTimeSetUp) {
-            const populateCamelArray = (camelArray: Camel[]) => {
-                const radix = 36
-                const seedPart = seed.slice(index * camelInformationLength, (1 + index) * camelInformationLength);
-                const maxLevel = (new DefaultLevelCurve()).maxSkillLevel;
+        const variation = 30;
+        const uniformCenters = Array.from(new Array(this._numberOfCamels), (e, i) => (100 / this._numberOfCamels) * (i + 1));
 
-                camelArray.push(
+        if (firstTimeSetUp) {
+            uniformCenters.forEach(center => {
+                camels.push(
                     this._camelCreator.createSeededCamel([
-                        maxLevel * parseInt(seedPart.slice(0,2), radix) / (radix ** 2),
-                        maxLevel * parseInt(seedPart.slice(2,4), radix) / (radix ** 2),
-                        maxLevel * parseInt(seedPart.slice(4,6), radix) / (radix ** 2),
-                        parseInt(seedPart.slice(6,7), radix) / radix,
-                        parseInt(seedPart.slice(7,8), radix) / radix,
-                        parseInt(seedPart.slice(8,9), radix) / radix,
-                        parseInt(seedPart.slice(8,9), radix) / radix,
+                        this.generateRandomNumber(center, variation),
+                        this.generateRandomNumber(center, variation),
+                        this.generateRandomNumber(center, variation),
+                        parseInt(this.generateSeed(1), this._seedRadix) / this._seedRadix,
+                        parseInt(this.generateSeed(1), this._seedRadix) / this._seedRadix,
+                        parseInt(this.generateSeed(1), this._seedRadix) / this._seedRadix,
+                        parseInt(this.generateSeed(1), this._seedRadix) / this._seedRadix,
                     ])
                 );
+            });
 
-                index += camelInformationLength;
-            };
-
-            new Array(30).forEach(quality => populateCamelArray(camels));
+            return;
         }
 
-        let easyCamelCount = 10;
-        let mediumCamelCount = 10;
-        let hardCamelCount = 10;
-        
-        let easyCamels: Camel[] = [];
-        let mediumCamels: Camel[] = [];
-        let hardCamels: Camel[] = [];
-
-        const populateQualityCamelArray = (quality: InitCamelQuality, camelArray: Camel[]) => {
+        const populateCamelArray = (camelArray: Camel[]) => {
             const radix = 36
-            const seedPart = seed.slice(index * camelInformationLength, (1 + index) * camelInformationLength);
+            const seedPart = seed.slice(index * this._camelInformationLength, (1 + index) * this._camelInformationLength);
+            const maxLevel = (new DefaultLevelCurve()).maxSkillLevel;
 
             camelArray.push(
                 this._camelCreator.createSeededCamel([
-                    ((quality + 1) / 10) * parseInt(seedPart.slice(0,2), radix) / (radix ** 2),
-                    ((quality + 1) / 10) * parseInt(seedPart.slice(2,4), radix) / (radix ** 2),
-                    ((quality + 1) / 10) * parseInt(seedPart.slice(4,6), radix) / (radix ** 2),
-                    parseInt(seedPart.slice(6,7), radix) / radix,
-                    parseInt(seedPart.slice(7,8), radix) / radix,
-                    parseInt(seedPart.slice(8,9), radix) / radix,
-                    parseInt(seedPart.slice(8,9), radix) / radix,
+                    maxLevel * parseInt(seedPart.slice(0, 2), radix) / (radix ** 2),
+                    maxLevel * parseInt(seedPart.slice(2, 4), radix) / (radix ** 2),
+                    maxLevel * parseInt(seedPart.slice(4, 6), radix) / (radix ** 2),
+                    parseInt(seedPart.slice(6, 7), radix) / radix,
+                    parseInt(seedPart.slice(7, 8), radix) / radix,
+                    parseInt(seedPart.slice(8, 9), radix) / radix,
+                    parseInt(seedPart.slice(9, 10), radix) / radix,
                 ])
             );
 
-            index += camelInformationLength;
+            index += this._camelInformationLength;
         };
 
-        new Array(easyCamelCount).fill(InitCamelQuality.High).forEach(quality => populateQualityCamelArray(quality, easyCamels));
-        new Array(mediumCamelCount).fill(InitCamelQuality.Cpu1).forEach(quality => populateQualityCamelArray(quality, mediumCamels));
-        new Array(hardCamelCount).fill(InitCamelQuality.Cpu5).forEach(quality => populateQualityCamelArray(quality, hardCamels));
+        new Array(this._numberOfCamels).forEach(quality => populateCamelArray(camels));
+
+        return;
     }
 
     public static GetGeneralWaste: () => Camel = () => {
@@ -90,7 +83,12 @@ class CamelStable {
     };
 
     private generateSeed(length: number = 700, radix: number = 36): string {
-        return "x".repeat(length).replace(/x/g, (char) => 
-            Math.floor(Math.random() * radix).toString(radix));  
+        return "x".repeat(length).replace(/x/g, (char) =>
+            Math.floor(Math.random() * radix).toString(radix));
+    }
+
+    private generateRandomNumber(center: number, plusMinusRange: number, max: number = 100, min: number = 0) {
+        const randValue = center + ((Math.random()) * 2 - 1) * plusMinusRange;
+        return Math.max(min, Math.min(max, randValue));
     }
 }
