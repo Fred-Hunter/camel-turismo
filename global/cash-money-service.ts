39d60b1553872701126266bb4d3dcfd1c533ad7e
Fred-Hunter <fredski.1+fredgithub@hotmail.co.uk>
2023-11-03 21:49:23 +0000

    Sorry, update cashmoney

--- global/cash-money-service.ts
+++ global/cash-money-service.ts
@@ -7,13 +7,18 @@ export class CashMoneyService {
         img.src = './assets/egyptian-pound.jpg';
         img.onload = function() {
             ctx.save();
-            ctx.drawImage(img, GlobalStaticConstants.innerWidth - 450, GlobalStaticConstants.innerHeight - 150, 400, 125);
-            ctx.fillStyle = '#e8be9e';
-            ctx.fillRect(GlobalStaticConstants.innerWidth - 375, GlobalStaticConstants.innerHeight - 125, 250, 25);
-            ctx.font = '30pt Garamond';
-            ctx.fillStyle = '#000';
+            ctx.drawImage(img, GlobalStaticConstants.innerWidth - 350, GlobalStaticConstants.innerHeight - 120, 300, 95);
+
+            ctx.font = 'bold 25pt Garamond';
+            ctx.fillStyle = GlobalStaticConstants.highlightColour;
+            ctx.strokeStyle = '#e8be9e'
+            ctx.lineWidth = 3;
             ctx.textAlign = "center";
-            ctx.fillText('Cash money: ' + GameState.cashMoney, GlobalStaticConstants.innerWidth - 250, GlobalStaticConstants.innerHeight - 102, 250);
+
+            const text: [string, number, number, number] = ['Cash money: ' + GameState.cashMoney, GlobalStaticConstants.innerWidth - 200, GlobalStaticConstants.innerHeight - 70, 300];
+
+            ctx.strokeText(...text);
+            ctx.fillText(...text);
             ctx.restore();
         }
     }
